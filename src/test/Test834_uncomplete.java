a82f6fefc2a46c661c5c85a435fe01967ac8c5a3
rokeabbey <rokeabbey@gmail.com>
2021-08-20 20:34:23 +0800

    834未完成，暂时提交。

--- src/test/Test834_uncomplete.java
+++ src/test/Test834_uncomplete.java
@@ -1,26 +1,35 @@
 package test;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
-public class Test834 {
+public class Test834_uncomplete {
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
         if (edges.length < 1) { return new int[]{0}; }
 
         HashMap<Integer, Node> nodes = new HashMap<>();
-        nodes.put(edges[0][0], new Node(-1, 1, 0, 2));
-        nodes.put(edges[0][1], new Node(edges[0][0], 1, 1, 2));
+        Integer[] indexes = new Integer[edges.length];
+        for (int i = 0; i < indexes.length; i ++) {
+            indexes[i] = i;
+        }
+        Arrays.sort(indexes, (o1, o2) -> {
+            int min0 = Math.min(edges[o1][0], edges[o1][1]);
+            int min1 = Math.min(edges[o2][0], edges[o2][1]);
+            if (min0 < min1) { return -1; }
+            else if (min0 > min1) { return 1; }
+            return 0;
+        });
+        nodes.put(edges[indexes[0]][0], new Node(-1, 1, 0, 2));
+        nodes.put(edges[indexes[0]][1], new Node(edges[indexes[0]][0], 1, 1, 2));
 
         int[] path = new int[n];
         int[] dis = new int[n];
         for (int i = 1; i < edges.length; i++) {
-            if (nodes.containsKey(edges[i][1])) {
-                swapOne(edges[i]);
+            if (nodes.containsKey(edges[indexes[i]][1])) {
+                swapOne(edges[indexes[i]]);
+//                j = 1;
             }
-            int parent = edges[i][0];
-            int me = edges[i][1];
+            int parent = edges[indexes[i]][0];
+            int me = edges[indexes[i]][1];
             int len = getPath(nodes, path, parent);
             promise(nodes, path, len);
             add(nodes, path, len, me);
@@ -28,29 +37,23 @@ public class Test834 {
 
         HashSet<Integer> notLeaves = new HashSet<>();
         for (int i = edges.length - 1; i >= 0; i--) {
-            if (notLeaves.contains(edges[i][1])) {
-                notLeaves.add(edges[i][0]);
+            if (notLeaves.contains(edges[indexes[i]][1])) {
+                notLeaves.add(edges[indexes[i]][0]);
                 continue;
             }
-            int len = getPath(nodes, path, edges[i][1]);
+            int len = getPath(nodes, path, edges[indexes[i]][1]);
             promise(nodes, path, len);
-            notLeaves.add(edges[i][0]);
+            notLeaves.add(edges[indexes[i]][0]);
         }
 
         for (int i = 0; i < edges.length; i++) {
+            dis[edges[i][0]] = nodes.get(edges[i][0]).dis;
             dis[edges[i][1]] = nodes.get(edges[i][1]).dis;
         }
-        dis[edges[0][0]] = nodes.get(edges[0][0]).dis;
+//        dis[edges[0][0]] = nodes.get(edges[0][0]).dis;
         return dis;
     }
 
-    public void swap(int[][] edges) {
-        for (int[] e: edges) {
-            if (e[0] > e[1]) {
-                swapOne(e);
-            }
-        }
-    }
 
     public void swapOne(int[] e) {
         int tmp = e[0];
@@ -128,7 +131,7 @@ public class Test834 {
     }
 
     public static void main(String[] args) {
-        Test834 t = new Test834();
+        Test834_uncomplete t = new Test834_uncomplete();
 //        int[][] edges = {{0, 1}, {0, 2}, {2, 3}, {2, 4}, {2, 5}};
 //        int n = 6;
 //        int[][] edges = {{0, 1}, {0, 2}};
@@ -139,8 +142,12 @@ public class Test834 {
 //        int[][] edges = {{2, 0}, {1, 0}};
 //        int n = 3;
 //        int[][] edges = {{2, 1}, {0, 2}};
-        int n = 9;
-        int[][] edges = {{1,0},{2,0}, {0,3}, {2,4}, {4,5},{3,6}, {8,7},{7,6}};
+//        int n = 9;
+//        int[][] edges = {{1,0},{2,0}, {0,3}, {2,4}, {4,5},{3,6},{8,7},{7,6}};
+
+        int n = 4;
+        int[][] edges = {{1, 2}, {3, 2}, {3, 0}};
+
 
         int[] result = t.sumOfDistancesInTree(n, edges);
         for (int i : result) {

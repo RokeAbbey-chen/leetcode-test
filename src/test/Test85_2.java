7569a98da9f940d2e973d99ba77b5ad3003e7fc9
rokeabbey <rokeabbey@gmail.com>
2021-08-20 22:36:20 +0800

    提交85

--- src/test/Test85_2.java
+++ src/test/Test85_2.java
@@ -11,178 +11,73 @@ public class Test85_2 {
             return 0;
         }
         int[][] heights = new int[2][matrix[0].length];
+        int res = 0;
+        int tmpRes = 0;
         for (int j = 0; j < matrix[0].length; j ++) {
-            heights[0][j] = matrix[0][j];
+            heights[0][j] = '1' == matrix[0][j] ? 1 : 0;
+            tmpRes = heights[0][j] == 1 ? heights[0][j] + tmpRes : 0;
+            res = Math.max(res, tmpRes);
         }
 
-        int res = 0;
         for (int i = 1; i < matrix.length; i ++){
             for (int j = 0; j < matrix[i & 1].length; j ++) {
-                if ('1' == matrix[i & 1][j])
+                if ('1' == matrix[i][j])
                     heights[i & 1][j] = heights[(i - 1) & 1][j] + 1;
                 else heights[i & 1][j] = 0;
             }
-            res = Math.max(res, largestRectangleArea(heights[i & 1]));
+
+            res = Math.max(res, getMaxArea(heights[i & 1]));
         }
         return res;
     }
 
-    public int largestRectangleArea(int[] heights) {
-        SegTree segTree = new SegTree(heights);
-        LinkedList<int[]> queue = new LinkedList<>();
-        int[] pair = new int[]{0, heights.length - 1};
-        queue.add(pair);
-        int maxArea = 0;
-        while (!queue.isEmpty()) {
-            pair = queue.removeFirst();
-            List<Integer> heightIndexes = segTree.findMinIndexes(pair[0], pair[1]);
-            maxArea = Math.max(heights[heightIndexes.get(0)] * (pair[1] - pair[0] + 1), maxArea);
-            int start = pair[0];
-            int end = -1;
-
-            Iterator<Integer> iter = heightIndexes.iterator();
-            assert heightIndexes.size() >= 1;
-            int[] pair2;
-
-            while (iter.hasNext()){
-                end = iter.next();
-                if (start <= end - 1) {
-                    pair2 = new int[]{start, end - 1};
-                    queue.addLast(pair2);
-                    start = end + 1;
-                } else {
-                    start = end + 1;
-                }
+    public int getMaxArea(int[] heights) {
+        int[] stack = new int[heights.length];
+        int size = 0;
+        int res = 0;
+        for (int i = 0; i < heights.length; i ++) {
+            while (size > 0 && heights[stack[size - 1]] >= heights[i]) {
+                size --;
             }
-            if (end == -1) continue;
-            start = end + 1;
-            end = pair[1];
-            if (start > end) continue;
-            pair2 = new int[]{start, end};
-            queue.addLast(pair2);
-        }
-        return maxArea;
-    }
-
-    static class SegTree {
-        SegTree.Node root;
-        int[] values;
-        public SegTree(int[] values) {
-            root = constructMinValueSegTree(values, 0, values.length - 1);
-            this.values = values;
-        }
-
-        public SegTree.Node constructMinValueSegTree(int[] values, int start, int end) {
-            if (start >= end) {
-                return new SegTree.Node(values[start], start);
-            }
-
-            int mid = (start + end) >> 1;
-            SegTree.Node leftNode = constructMinValueSegTree(values, start, mid);
-            SegTree.Node rightNode = constructMinValueSegTree(values,mid + 1, end);
-            SegTree.Node self = new SegTree.Node();
-            SegTree.Node tmp;
-            if (leftNode.value <= rightNode.value) tmp = leftNode;
-            else tmp = rightNode;
-            self.value = tmp.value;
-            self.addAllIndexes(tmp);
-
-            if (leftNode.value == rightNode.value) {
-                self.addAllIndexes(rightNode);
-            }
-
-            self.left = leftNode;
-            self.right = rightNode;
-            return self;
-        }
-
-        public List<Integer> findMinIndexes(int start, int end) {
-            return findMinIndexes(start, end, root, 0, values.length - 1);
-        }
-
-        public List<Integer> findMinIndexes(int start, int end, SegTree.Node node, int nodeStart, int nodeEnd) {
-            if (start <= nodeStart && end >= nodeEnd) {
-                return node.indexes;
-            }
-            int mid = (nodeStart + nodeEnd) >> 1;
-            if (start > mid) {
-                return findMinIndexes(start, end, node.right, mid + 1, nodeEnd);
-            }
-            if (end <= mid) {
-                return findMinIndexes(start, end, node.left, nodeStart, mid);
-            }
-
-            List<Integer> leftMinIndexes, rightMinIndexes;
-            leftMinIndexes = findMinIndexes(start, mid, node.left, nodeStart, mid);
-            rightMinIndexes = findMinIndexes(mid + 1, end, node.right, mid + 1, nodeEnd);
-            int leftIndex = leftMinIndexes.get(0);
-            int rightIndex = rightMinIndexes.get(0);
-            if (values[leftIndex] < values[rightIndex]) {
-                return leftMinIndexes;
-            } else if (values[leftIndex] > values[rightIndex]) {
-                return rightMinIndexes;
+            if (0 == size) {
+                res = Math.max(res, (i + 1) * heights[i]);
             } else {
-                LinkedList<Integer> tmp = new LinkedList<>();
-                tmp.addAll(leftMinIndexes);
-                tmp.addAll(rightMinIndexes);
-                return tmp;
+                res = Math.max(res, (i - stack[size - 1]) * heights[i]);
+            }
+            stack[size ++ ] = i;
+
+            for (int size2 = size; size2 > 0; size2 --) {
+                if (size2 - 2 >= 0) {
+                    res = Math.max(res, (i - stack[size2 - 2]) * heights[stack[size2 - 1]]);
+                } else {
+                    res = Math.max(res, (i + 1) * heights[stack[size2 - 1]]);
+                }
             }
         }
-
-        static class Node {
-            int value;
-            List<Integer> indexes = new LinkedList<>();
-            SegTree.Node left;
-            SegTree.Node right;
-
-            public Node() {
-
-            }
-
-            public Node(int value, int selfIndex) {
-                this(value, selfIndex, null, null);
-            }
-
-            public Node(int value, int selfIndex, SegTree.Node left, SegTree.Node right) {
-                this.value = value;
-                this.left = left;
-                this.right = right;
-                indexes.add(selfIndex);
-            }
-
-            public void addIndex(int index) {
-                if (indexes == null) {
-                    indexes = new LinkedList<>();
-                }
-                indexes.add(index);
-            }
-
-            public void addAllIndexes(List<Integer> indexes) {
-                if (this.indexes == null) {
-                    indexes = new LinkedList<>();
-                }
-                this.indexes.addAll(indexes);
-            }
-
-            public void addAllIndexes(SegTree.Node node) {
-                addAllIndexes(node.indexes);
-            }
-        }
+        return res;
     }
 
-//    public int getMax
-//    public int getMaxArea(int[] heights) {
-//        int[] stack = new int[heights.length];
-//        int ids = 0;
-//        int res = 0;
-//        for (int i = 0; i < heights.length; i ++) {
-//            while (ids > 0 && heights[stack[ids - 1]] >= heights[i]) {
-//                res = Math.max(res, (i - stack[ids - 1] + 1) * heights[i]);
-//                ids --;
-//            }
-//            stack[ids ++ ] = [i];
-//        }
-//        return res;
-//    }
+    public static void main1(String[] args) {
+        Test85_2 test = new Test85_2();
+//        int[] heights = {3, 2, 1, 2, 3, 7, 8, 5, 3, 2, 8, 3, 8, 2, 9};
+        int[] heights = {3, 1, 2, 9, 4, 2, 1, 2, 5, 7, 7 , 12, 9, 97, 88, 54, 32, 12};
+//        int[] heights = {3, 2, 1, 2, 3, 7};
+        int res = test.getMaxArea(heights);
+
+        System.out.println("res = " + res);
+    }
+
+    public static void main2(String[] args) {
+        Test85_2 test = new Test85_2();
+//        char[][] matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+//        char[][] matrix = {{'1'}};
+        char[][] matrix = {{'0','1','1','0','1'},{'1','1','0','1','0'},{'0','1','1','1','0'},{'1','1','1','1','0'},{'1','1','1','1','1'},{'0','0','0','0','0'}};
+        int res = test.maximalRectangle(matrix);
+        System.out.println("res = " + res);
+    }
+
+    public static void main(String[] args) {
+        main1(args);
+    }
 
 }

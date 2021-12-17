0130f8936635f1d7eb74d067eb76e8c260b5e2f0
luoke <luoke.chen@mishudata.com>
2021-12-17 17:18:50 +0800

    ac Test282

--- src/test/Test282_2.java
+++ src/test/Test282_2.java
@@ -5,32 +5,51 @@ import java.util.List;
 
 public class Test282_2 {
     public List<String> addOperators(String num, int target) {
-
+        ArrayList<String> ans = new ArrayList<>();
+        help(num, 0, 0, 0, "", ans, target);
+        return ans;
     }
 
-    public void help(String exp, int target, long wholeValue,
-                     long firstFactor, long secondFactor, char lastOp, List<String> ans) {
-        if (wholeValue == target) ans.add(exp);
+    public void help(String num, int start, long last0, long last1, String exp, ArrayList<String> ans, long target){
+//        System.out.println("exp:" + exp + ", = " + last0);
+        int len = num.length();
+        if (last0 == target && start == len)
+            ans.add(exp);
 
-        long deno = (long)1e10;
-        char[] ops = {'+', '-', '*'};
-        while (deno > 0) {
-            long f = secondFactor / deno;
-            long s = secondFactor % deno;
-            if (f == 0) continue;
-            wholeValue -= calc(firstFactor, secondFactor, lastOp, false);
-            for (char op : ops) {
-                long newWhole = wholeValue + calc(firstFactor, f,)
+        for (int i = start + 1; i <= len; i ++) {
+            String operand = num.substring(start, i);
+            if (operand.length() >= 2 && '0' == operand.charAt(0)) continue;
+            long operandInt = Long.parseLong(operand);
+
+            if (0 == start) {
+                help(num, i,last0 + operandInt, operandInt, operand, ans, target);
+            } else {
+                help(num, i,last0 + operandInt, operandInt, exp + '+' + operand, ans, target);
+                help(num, i,last0 - operandInt, -operandInt, exp + '-' + operand, ans, target);
+                help(num, i, last0 - last1 + last1 * operandInt,
+                        last1 * operandInt, exp + '*' + operand, ans, target);
             }
         }
     }
 
-    public long calc(long first, long second, char lastOp, boolean reverse) {
-        switch (lastOp) {
-            case '+': return !reverse ? first + second : first - second;
-            case '-': return !reverse ? first - second : first + second;
-            default : return !reverse ? first * second : first / second;
-        }
+
+
+    public static void main(String[] args) {
+        String num = "28920032";
+        int target = 124;
+//        String num = "003";
+//        int target = 3;
+
+//        String num = "3456237490";
+//        int target = 9197;
+
+        Test282_2 t = new Test282_2();
+        Test282 t2 = new Test282();
+        List<String> result = t.addOperators(num, target);
+        System.out.println(result);
+
+        List<String> result2 = t2.addOperators(num, target);
+        System.out.println(result2);
     }
 
 }

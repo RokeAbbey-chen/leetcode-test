5a250d1d2745e07e7bfbab57bff113b8ae37f16c
luoke <luoke.chen@mishudata.com>
2022-01-27 17:34:49 +0800

    ac 391

--- src/test/Test391.java
+++ src/test/Test391.java
@@ -1,9 +1,6 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Test391 {
     public boolean isRectangleCover(int[][] rectangles) {

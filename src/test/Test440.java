0c8151153db04a2ae3062c5e8e30ed3c8136f7a1
luoke <luoke.chen@mishudata.com>
2022-06-01 17:01:19 +0800

    ac 440

---
+++ src/test/Test440.java
@@ -0,0 +1,60 @@
+package test;
+
+public class Test440 {
+    public int findKthNumber(int n, int k) {
+
+        int n2 = n;
+        int n3 = 1;
+        int numDigits = 0;
+        int T = 0;
+        while (n2 > 0) {
+            numDigits ++;
+            n2 /= 10;
+            n3 *= 10;
+            T = 10 * T + 1;
+        }
+        n3 /= 10;
+
+        int rank = 0;
+        int jinweiTime = 0;
+        int result = 0;
+
+        while (T > 0) {
+            if (jinweiTime < numDigits && result * 10 <= n) {
+                result = 10 * result;// jinweiTime == 0? 1 : 10 * result;
+                rank ++;
+                jinweiTime ++;
+            }
+
+            int i = jinweiTime == 1 ? 1 : 0;
+            for (; rank <= k && i < 10; i ++){
+//                System.out.println("rank = " + rank + ", i = " + i);
+//                int firstNum = n % (10 * n3) / n3;
+                if (i < 9 &&
+                    result * n3 + i * n3 >= n / n3 * n3 &&
+                    rank + T - (1 * n3 - 1 - n % n3) <= k) {
+                        rank += T - (1 * n3 - 1 - n % n3);//T - (n3 - 1 - n % n3);
+                        T /= 10;
+                        n3 /= 10;
+//                        System.out.println("T down. T = " + T + ", i = " + i);
+                } else rank += T;
+            }
+            rank -= T;
+            i --;
+            result += i;
+//            System.out.println("rank = " + rank + ", result = " + result + ", i = " + i);
+            if (rank == k) {
+                return result;
+            }
+            T /= 10;
+            n3 /= 10;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Test440 t = new Test440();
+        System.out.println(t.findKthNumber(739, 739));
+    }
+}

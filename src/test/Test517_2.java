924ec262d31114818016c20bb8bfd33b23cde459
rokeabbey <CoddyAbbey888888*>
2022-07-11 23:06:34 +0800

    517

--- src/test/Test517_2.java
+++ src/test/Test517_2.java
@@ -23,11 +23,12 @@ public class Test517_2 {
         sum = 0;
         int exclude = 0;
         int rest = 0;
-        int left = 0;
-        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4, 24};
+        int left = 0, right = 0;
+//        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4, 24};
         for (int i = 0; i < n; i ++) {
             needFill += Math.max(avg - mac[i], 0);
             needMove += Math.max(mac[i] - avg, 0);
+            System.out.println("needFill = " + needFill + ", needMove = " + needMove + ", mac[" + i + "] = " + mac[i]);
             if (sum < i * avg && (sum + mac[i] >= (i + 1) * avg
                     || (i + 1) < n && mac[i] >= avg && mac[i + 1] < avg)) {
                 if (sum + mac[i] > (i + 1) * avg) {
@@ -41,17 +42,19 @@ public class Test517_2 {
                 needFill -= realMove;
                 needMove -= realMove;
                 needFill0 += needFill;
-                needMove0 += needMove;
-                needFill = 0; needMove = 0;
+//                needMove0 += needMove;
+                needFill = 0; // needMove = 0;
             } else if (sum > i * avg && (sum + mac[i] <= (i + 1) * avg
                     || (i + 1) < n && mac[i] <= avg && mac[i + 1] > avg)) {
                 int realMove = Math.min(needFill, needMove + needMove0);
+                right = realMove + Math.max(0, right - Math.min(needMove, realMove));
+                moveTimes = Math.max(right, moveTimes);
                 moveTimes = Math.max(moveTimes, Math.max(realMove - rest, exclude) + rest);
                 needMove -= realMove;
                 needFill -= realMove;
                 needMove0 += needMove;
-                needFill0 += needFill;
-                needFill = 0; needMove = 0;
+//                needFill0 += needFill;
+                needMove = 0;
                 if (realMove >= exclude) rest = Math.max(rest + exclude - realMove, 0);
                 if (0 == rest) exclude = 0;
             }
@@ -69,9 +72,12 @@ public class Test517_2 {
 //        int[] nums = {0, 4, 12, 0};
 //        int[] nums = {0, 3, 10, 2, 7, 9, 4};
 //        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4};
-        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4, 24};
+//        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4, 24};
 //        int[] nums = {0,0,10,0,0,0,10,0,0,0};
 //        int[] nums = {0,0,14,0,10,0,0,0};
+//        int[] nums = {91,95,31,48,28,41,32,29,81,4};
+//        int[] nums = {44,46,11,2,12,64,40,60,92,9};
+        int[] nums = {50,62,75,31,2,84,20,74,49,73};
         int result = t.findMinMoves(nums);
 
         System.out.println("result = " + result);

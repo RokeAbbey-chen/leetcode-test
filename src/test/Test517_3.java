82c5372374d2cba7c51bebf657423bffaab47eb6
luoke <luoke.chen@mishudata.com>
2022-07-15 21:00:30 +0800

    517

--- src/test/Test517_3.java
+++ src/test/Test517_3.java
@@ -27,36 +27,51 @@ public class Test517_3 {
         int rest0 = 0;
         for (int i = 0; i < n; i ++) {
 
+            System.out.println("i = " + i + ", mac[i] = " + mac[i]);
             sum00 += Math.max(0, avg - mac[i]);
             sum11 += Math.max(0, mac[i] - avg);
 
-            System.out.println("sum00 = " + sum00 + ", sum11 = " + sum11);
+            System.out.println("sum0=" + sum0 +", sum00=" + sum00 + ", sum1=" + sum1 + ", sum11=" + sum11);
             if (sum0 > sum1 && sum0 + sum00 <= sum1 + sum11) {
-                System.out.println("1. sum0 = " + sum0 + ", sum1 = " + sum1);
+                System.out.println("W1. sum0 = " + sum0 + ", sum1 = " + sum1);
                 int rest = sum1 + sum11 - sum0 - sum00;
                 exclusive = mac[i] - rest - avg;
                 queue.add(exclusive);
-                result = Math.max(result, solve(queue));
+                int solveTimes = solve(queue);
+                System.out.println("result = " + result + ", solveTimes = " + solveTimes);
+                result = Math.max(result, solveTimes);
+                System.out.println("result = " + result);
+                queue.clear();
                 sum0 = sum00 = sum1 = 0;
                 sum11 = rest;
                 rest0 = rest;
             } else if (sum1 > sum0 && sum1 + sum11 <= sum0 + sum00) {
-                System.out.println("2. sum0 = " + sum0 + ", sum1 = " + sum1);
+                System.out.println("W2. sum0 = " + sum0 + ", sum1 = " + sum1);
                 int rest = sum0 + sum00 - sum1 - sum11;
                 queue.add(mac[i] - rest - avg);
                 int solveTimes = solve(queue);
+                System.out.println("result = " + result + ", solveTimes = " + solveTimes);
+                queue.clear();
                 result = Math.max(result, Math.max(solveTimes - rest0, exclusive) + rest0);
+                System.out.println("result = " + result);
                 sum1 = sum11 = sum0 = 0;
                 sum00 = rest;
-            } else if ((mac[i] > avg || n - 1 == i && avg == mac[i]) && sum00 > 0){
-                System.out.println("3. sum00 = " + sum00);
+            } else if ((i + 1 < n && mac[i + 1] > avg || n - 1 == i) && sum00 > 0){
+                System.out.println("W3. sum00 = " + sum00);
                 queue.add(sum00);
+                sum0 += sum00;
                 sum00 = 0;
-            } else if ((mac[i] < avg || n - 1 == i && avg == mac[i]) && sum11 > 0) {
-                System.out.println("4. sum11 = " + sum11);
+            } else if ((i + 1 < n && mac[i + 1] < avg || n - 1 == i) && sum11 > 0) {
+                System.out.println("W4. sum11 = " + sum11);
                 queue.add(sum11);
+                sum1 += sum11;
                 sum11 = 0;
             }
+            System.out.println("--------------------");
+        }
+//        int[] nums = {50,62,75,31,2,84,20,74,49,73};
+        if (queue.size() > 0) {
+            result = Math.max(solve(queue), result);
         }
         return result;
     }
@@ -72,6 +87,7 @@ public class Test517_3 {
                 int v = queue.get(i);
                 if (v == min) {
                     queue.remove(i);
+                    size --;
                     if (i >= 1 && i < size - 1) {
                         queue.set(i - 1, queue.get(i) + queue.get(i - 1));
                     }
@@ -95,7 +111,18 @@ public class Test517_3 {
 
     public static void main(String[] args) {
         Test517_3 t = new Test517_3();
-        int[] nums = {0, 3, 0};
+//        int[] nums = {0, 3, 0};
+        //        int[] nums = {0, 3, 0};
+//        int[] nums = {4, 0, 0, 4};
+//        int[] nums = {0, 4, 12, 0};
+//        int[] nums = {0, 3, 10, 2, 7, 9, 4};
+//        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4};
+//        int[] nums = {1, 0, 5, 7, 9, 10, 12, 4, 24};
+//        int[] nums = {0,0,10,0,0,0,10,0,0,0};
+//        int[] nums = {0,0,14,0,10,0,0,0};
+//        int[] nums = {91,95,31,48,28,41,32,29,81,4};
+//        int[] nums = {44,46,11,2,12,64,40,60,92,9};
+        int[] nums = {50,62,75,31,2,84,20,74,49,73};
         System.out.println(t.findMinMoves(nums));
     }
 }

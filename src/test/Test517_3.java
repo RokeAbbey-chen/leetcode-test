dd9919ee90f209fa198b3d69533f6035660f8d0c
luoke <luoke.chen@mishudata.com>
2022-07-15 20:35:30 +0800

    517

--- src/test/Test517_3.java
+++ src/test/Test517_3.java
@@ -1,7 +1,6 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Test517_3 {
     public int findMinMoves(int[] machines) {
@@ -19,90 +18,117 @@ public class Test517_3 {
 
         System.out.println("avg = " + avg);
 
+        ArrayList<Integer> queue = new ArrayList<>();
+        int sum0 = 0, sum1 = 0;
+        int sum00 = 0, sum11 = 0;
+        int result = 0;
 
-    }
+        int exclusive = 0;
+        int rest0 = 0;
+        for (int i = 0; i < n; i ++) {
 
-    public int fill(int[] mac, int avg, int macStart) {
-        int n = mac.length;
-        LinkedList<int[]> queue = new LinkedList<>();
-        int needFill = 0, needMove = 0;
+            sum00 += Math.max(0, avg - mac[i]);
+            sum11 += Math.max(0, mac[i] - avg);
 
-        boolean bFillOrMove = mac[macStart] < avg;
-
-        int sumFill = 0, sumMove = 0;
-        for (int i = macStart; i <= n; i ++) {
-            if (needFill > 0 && (i >= n || mac[i] > avg)) {
-                queue.add(new int[] {needFill, i - 1});
-                sumFill += needFill;
-                needFill = 0;
+            System.out.println("sum00 = " + sum00 + ", sum11 = " + sum11);
+            if (sum0 > sum1 && sum0 + sum00 <= sum1 + sum11) {
+                System.out.println("1. sum0 = " + sum0 + ", sum1 = " + sum1);
+                int rest = sum1 + sum11 - sum0 - sum00;
+                exclusive = mac[i] - rest - avg;
+                queue.add(exclusive);
+                result = Math.max(result, solve(queue));
+                sum0 = sum00 = sum1 = 0;
+                sum11 = rest;
+                rest0 = rest;
+            } else if (sum1 > sum0 && sum1 + sum11 <= sum0 + sum00) {
+                System.out.println("2. sum0 = " + sum0 + ", sum1 = " + sum1);
+                int rest = sum0 + sum00 - sum1 - sum11;
+                queue.add(mac[i] - rest - avg);
+                int solveTimes = solve(queue);
+                result = Math.max(result, Math.max(solveTimes - rest0, exclusive) + rest0);
+                sum1 = sum11 = sum0 = 0;
+                sum00 = rest;
+            } else if ((mac[i] > avg || n - 1 == i && avg == mac[i]) && sum00 > 0){
+                System.out.println("3. sum00 = " + sum00);
+                queue.add(sum00);
+                sum00 = 0;
+            } else if ((mac[i] < avg || n - 1 == i && avg == mac[i]) && sum11 > 0) {
+                System.out.println("4. sum11 = " + sum11);
+                queue.add(sum11);
+                sum11 = 0;
             }
-
-            if (needMove > 0 && (i >= n || mac[i] < avg)) {
-                queue.add(new int[] {needMove, i - 1});
-                sumMove += needMove;
-                needMove = 0;
-            }
-
-            if (bFillOrMove && sumMove >= sumFill || !bFillOrMove && sumFill >= sumMove) {
-                solve(mac, queue, bFillOrMove);
-            }
-            if (i < n) {
-                int stepFill = Math.max(0, avg - mac[i]);
-                int stepMove = Math.max(0, mac[i] - avg);
-                needFill += stepFill;
-                needMove += stepMove;
-            }
-
         }
+        return result;
     }
 
 
-    public int[] solve(int[] mac, LinkedList<int[]> queue, boolean flag) {
-        int[] result = {0, 0, 0};
+    public int solve(ArrayList<Integer> queue) {
 
-        LinkedList<int[]> queue2 = new LinkedList<>();
-        int maxMinTimes = 0;
-        int minTimes = Integer.MAX_VALUE;
-        while (queue.size() >= 2) {
-            int[] node1 = queue.removeLast();
-            int[] node0 = queue.removeLast();
-            minTimes = Math.min(Math.min(node0[0], node1[0]), minTimes);
-
-
-
+        int result = 0;
+        while (queue.size() > 0) {
+            int min = Collections.min(queue);
+            int size = queue.size();
+            for (int i = size - 1; i >= 0; i--) {
+                int v = queue.get(i);
+                if (v == min) {
+                    queue.remove(i);
+                    if (i >= 1 && i < size - 1) {
+                        queue.set(i - 1, queue.get(i) + queue.get(i - 1));
+                    }
+                } else {
+                    queue.set(i, v - min);
+                }
+            }
+            result += min;
         }
+        return result;
     }
 
+    public static void main1(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(0, 2, 4, 8, 6, 10, 12));
 
+        Test517_3 t = new Test517_3();
+        System.out.println(t.solve(list));
+//        System.out.println(t.findMinMoves(list));
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        Test517_3 t = new Test517_3();
+        int[] nums = {0, 3, 0};
+        System.out.println(t.findMinMoves(nums));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

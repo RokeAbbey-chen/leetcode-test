315386861f6761bd74e6cb5b929f95c662afad68
luoke <luoke.chen@mishudata.com>
2022-07-27 20:16:53 +0800

    ac 546

--- src/test/Test546.java
+++ src/test/Test546.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.HashSet;
+
 /**
  * Given several boxes with different colors represented by different positive numbers.
  You may experience several rounds to remove boxes until there is no box left. Each time you can choose some continuous boxes with the same color (composed of k boxes, k >= 1), remove them and get k*k points.
@@ -30,6 +32,35 @@ package test;
 public class Test546 {
 
     public int removeBoxes(int[] boxes) {
-        return 0;
+
+    }
+
+    public int remove(int[] boxes, boolean[] bUsed, byte[][] colorWhere, byte start, byte end) {
+        if (end < start) return 0;
+        if (end == start) return 1;
+        int maxSum = 0;
+        for (byte i = start; i <= end; i ++) {
+            byte color = (byte)boxes[i];
+            if (bUsed[color]) continue;
+            bUsed[color] = true;
+            int sum = 0;
+            byte j;
+            for (j = 0; j < colorWhere[color].length && colorWhere[color][j] < start; j ++);
+
+            byte j0 = (byte)(start - 1);
+            int count = j;
+            for (;j < colorWhere[color].length && colorWhere[color][j] <= end; j ++) {
+                byte j1 = colorWhere[color][j];
+                sum += remove(boxes, bUsed, colorWhere, (byte)(j0 + 1), (byte) (j1 - 1));
+                j0 = j1;
+            }
+            count = j - count + 1;
+            sum += count * count;
+            maxSum = Math.max(maxSum, sum);
+            bUsed[color] = false;
+        }
+        return maxSum;
+
+
     }
 }

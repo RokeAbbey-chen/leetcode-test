c359024e1576fcde3fa5138314b9ba488f577040
luoke <luoke.chen@mishudata.com>
2023-02-12 18:13:00 +0800

    ac 699 try 691 730

--- src/test/Test730.java
+++ src/test/Test730.java
@@ -8,10 +8,14 @@ public class Test730 {
         int[][] dp = new int[len][len];
         int result = 0;
         System.out.println("s = " + s);
+        boolean[] flags = {false, false, false, false};
         for (int i = 0; i < len; i ++) {
             dp[i][i] = 1;
-            result += dp[i][i];
-            result %= MOD;
+            if (!flags[chs[i] - 'a']) {
+                result += dp[i][i];
+                result %= MOD;
+                flags[chs[i] - 'a'] = true;
+            }
             System.out.println("s = " + s + ", chs[i] = " + chs[i]);
             System.out.println("i = " + i + ", result = " + result + ", dpii = " + dp[i][i]);
             int k = i;
@@ -21,18 +25,25 @@ public class Test730 {
                     int count = i - 1 >= j + 1 ? dp[i - 1][j + 1] : 0;
                     count = (count + 1) % MOD;
                     result += count;
-                    if (k + 1 <= i - 1) {
+                    System.out.println("chs[j] = " + chs[j] + ", count = " + count);
+                    if (k != i) {
+                        if (k + 1 <= i - 1) {
 
-                        result = (result - dp[i - 1][k + 1]) % MOD;
-                        result %= MOD;
-                    }
-                    if (j + 1 <= k - 1) {
-                        result = (result - dp[j + 1][k - 1]) % MOD;
-                        result %= MOD;
+                            System.out.println("dpik = " + dp[i - 1][k + 1]);
+                            result = (result - dp[i - 1][k + 1]) % MOD;
+                            result %= MOD;
+                            System.out.println("result = " + result);
+                        }
+                        if (j + 1 <= k - 1) {
+                            System.out.println("dpjk = " + dp[j + 1][k - 1]);
+                            result = (result - dp[j + 1][k - 1]) % MOD;
+                            result %= MOD;
+                            System.out.println("result = " + result);
+                        }
+//                        System.out.println("chs[j] = " + chs[j] + ", dpij = " + dp[i][j] + ", dp[i][k] = " + dp[i][k] + ", dp[j][k] = " + dp[j][k] + ", result = " + result);
                     }
 //                    result += (((count - dp[i][k]) % MOD - dp[j][k]) % MOD + 1) % MOD;
 //                    result %= MOD;
-                    System.out.println("chs[j] = " + chs[j] + ", dpij = " + dp[i][j] + ", dp[i][k] = " + dp[i][k] + ", dp[j][k] = " + dp[j][k] + ", result = " + result);
                     dp[i][j] = dp[j][i] = count;
                     k = j;
                 } else {
@@ -48,7 +59,7 @@ public class Test730 {
 
     public static void main(String[] args) {
         Test730 t = new Test730();
-        String s = "abccdc";
+        String s = "abccdca";
         int result = t.countPalindromicSubsequences(s);
         System.out.println("result:" + result);
     }
